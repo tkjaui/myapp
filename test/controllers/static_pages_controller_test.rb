56e533eb3917b703e6e1d1102860ff6d8f052e07
takuji <tkjaui@gmail.com>
2020-09-14 21:26:15 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "データ復旧シェアサービス"
   end
 
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -21,6 +26,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About|#{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact|#{@base_title}"
   end
 
 end
