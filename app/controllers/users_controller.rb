c0559ef5f571b3d0006ced87aab5bdbdd03db6dc
takuji <tkjaui@gmail.com>
2020-09-18 19:10:35 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to データ復旧シェアサービス!"
       redirect_to @user
     else
