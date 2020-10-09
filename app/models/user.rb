4445c7ec3657a67a994743f79996e75b9a7de789
takuji <tkjaui@gmail.com>
2020-10-09 16:53:52 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -49,6 +50,10 @@ class User < ApplicationRecord
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def feed
+    Micropost.where("user_id=?", id)
   end
   
   private
